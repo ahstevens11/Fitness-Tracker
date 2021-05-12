efc6de2080b96f1930a3f3f40fe78c47eaf8bbee
ahstevens11 <austin.cards20@gmail.com>
2021-05-12 15:56:12 -0500

    api routines finished

--- api/routines.js
+++ api/routines.js
@@ -5,6 +5,9 @@ const {
     getAllPublicRoutines,
     getRoutineById,
     destroyRoutine,
+    createRoutine,
+    updateRoutine,
+    addActivityToRoutine
 } = require("../db");
 
 routinesRouter.get('/', async (req, res, next) => {
@@ -16,29 +19,56 @@ routinesRouter.get('/', async (req, res, next) => {
     }
 })
 
-routinesRouter.get('/', requireUser, async (req, res, next) => {
+routinesRouter.post('/', requireUser, async (req, res, next) => {
+    const {isPublic, name, goal} = req.body;
+    const {id} = req.user;
+    const creatorId = id
     try {
+        const routine = await createRoutine({creatorId, isPublic, name, goal})
 
+        res.send(routine)
     } catch (error) {
         next(error)
     }
 })
 
+routinesRouter.patch('/:routineId', requireUser, async (req, res, next) => {
+    try {
+        const {isPublic, name, goal} = req.body;
+        const {routineId} = req.params;
+        const routine = await updateRoutine({id: routineId, isPublic, name, goal})
+        res.send(routine)
+    } catch (error) {
+        next(error)
+    }
+})
 
+routinesRouter.delete('/:routineId', requireUser, async (req, res, next) => {
+    try {
+        const {routineId} = req.params
+        const routine = await getRoutineById(routineId)
+        if (routine.creatorId === req.user.id) { //in the if statement it should be something for routineId === id
+            const deletedRoutine = await destroyRoutine(routineId)
+            res.send(deletedRoutine)
+        }
+    } catch (error) {
+        next(error)
+    }
+})
 
-// routinesRouter.delete('/:routineId', requireUser, async (req, res, next) => {
-//     try {
-//         const _routine = await getRoutineById(req.params.routineId)
-
-//         if (_routine && _routine.creatorId === req.user.username) {
-//             await destroyRoutine(req.params.routineId)
-//             res.send({message: "Routine was deleted"})
-//         } else {
-//             throw new Error("Routine could not be deleted")
-//         }
-//     } catch (error) {
-//         next(error)
-//     }
-// })
+routinesRouter.post('/:routineId/activities', requireUser, async (req, res, next) => {
+    try {  
+        const {activityId, count, duration} = req.body;
+        const {routineId} = req.params; 
+        const routine_activity = await addActivityToRoutine({routineId, activityId, count, duration})
+        if(routine_activity) {
+            res.send(routine_activity)
+        } else {
+            next({message: "duplication on activityId and routineId"})
+        }
+    } catch (error) {
+        next(error)
+    }
+})
 
 module.exports = routinesRouter
